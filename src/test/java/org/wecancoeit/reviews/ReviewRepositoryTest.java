cef291144c9ce44df2a951c5648c1e7a110aa882
Troublesm1 <Jovahnn@yahoo.com>
2021-05-28 16:17:50 -0400

    Code CleanUp

--- src/test/java/org/wecancoeit/reviews/ReviewRepositoryTest.java
+++ src/test/java/org/wecancoeit/reviews/ReviewRepositoryTest.java
@@ -2,25 +2,28 @@ package org.wecancoeit.reviews;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Collection;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Collection;
+
 public class ReviewRepositoryTest {
+
+    private ReviewRepository underTest;
+    private Review reviewOne = new Review(1L, "review one name", "pic one", "review one category", "synopsis", "date");
+    private Review reviewTwo = new Review(2L, "review two name", "pic two", "review two category", "synopsis", "date");
+
 
     @Test
     public void shouldFindReviewOne() {
-        Review reviewOne = new Review (1L, "review one name", "pic one", "review one category", "synopsis", "date");
-        ReviewRepository underTest = new ReviewRepository(reviewOne);
+        underTest = new ReviewRepository(reviewOne);
         Review foundReview = underTest.findOne(1L);
         assertEquals(reviewOne, foundReview);
     }
+
     @Test
     public void shouldFindReviewOneAndReviewTwo() {
-        Review reviewOne = new Review (1L, "review one name", "pic one", "review one category", "synopsis", "date");
-        Review reviewTwo = new Review (2L, "review two name", "pic two", "review two category", "synopsis", "date");
-        ReviewRepository underTest = new ReviewRepository(reviewOne, reviewTwo );
+        underTest = new ReviewRepository(reviewOne, reviewTwo);
         Collection<Review> foundReviews = underTest.findAll();
         assertThat(foundReviews).contains(reviewOne, reviewTwo);
     }
