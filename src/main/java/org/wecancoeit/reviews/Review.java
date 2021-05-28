e03fd591d631daa313ff73d5ba25a8a2dc235212
Troublesm1 <Jovahnn@yahoo.com>
2021-05-28 11:30:56 -0400

    Review class started

---
+++ src/main/java/org/wecancoeit/reviews/Review.java
@@ -0,0 +1,15 @@
+package org.wecancoeit.reviews;
+
+public class Review {
+
+    private Long id;
+    private String name;
+    private String pic;
+    private String category;
+    private String synopsis;
+    private String date;
+
+
+    public Review(long l, String review_one_name, String pic_one, String review_one_category, String synopsis, String date) {
+    }
+}
---
+++ src/test/java/org/wecancoeit/reviews/ReviewRepositoryTest.java
@@ -0,0 +1,13 @@
+package org.wecancoeit.reviews;
+
+import org.junit.jupiter.api.Test;
+
+public class ReviewRepositoryTest {
+
+    @Test
+    public void shouldFindReviewOne() {
+        Review reviewOne = new Review (1L, "review one name", "pic one", "review one category", "synopsis", "date");
+
+    }
+
+}
