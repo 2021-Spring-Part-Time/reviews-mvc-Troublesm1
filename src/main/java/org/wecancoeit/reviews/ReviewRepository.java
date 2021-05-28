fdcf4b20abe21d668d46d98cc35437b714ab12cb
Troublesm1 <Jovahnn@yahoo.com>
2021-05-28 12:21:55 -0400

    ReviewRepository class started

---
+++ src/main/java/org/wecancoeit/reviews/ReviewRepository.java
@@ -0,0 +1,10 @@
+package org.wecancoeit.reviews;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ReviewRepository {
+
+    public ReviewRepository(Review reviewOne) {
+    }
+}
