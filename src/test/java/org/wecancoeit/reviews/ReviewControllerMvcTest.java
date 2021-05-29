6b0a682331c684886c659f98bb291e509b153262
Troublesm1 <Jovahnn@yahoo.com>
2021-05-28 21:07:44 -0400

    Review Controller Started

---
+++ src/test/java/org/wecancoeit/reviews/ReviewControllerMvcTest.java
@@ -0,0 +1,8 @@
+package org.wecancoeit.reviews;
+
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest (ReviewController.class)
+public class ReviewControllerMvcTest {
+}
