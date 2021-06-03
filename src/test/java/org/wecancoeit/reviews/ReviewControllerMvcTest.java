4f1378663a3af29f8a9f0ed57c2ad618d5304e72
Jovahnn Tucker <Jovahnn@yahoo.com>
2021-06-02 21:06:13 -0400

    Controller started

--- src/test/java/org/wecancoeit/reviews/ReviewControllerMvcTest.java
+++ src/test/java/org/wecancoeit/reviews/ReviewControllerMvcTest.java
@@ -1,8 +1,26 @@
 package org.wecancoeit.reviews;
 
-
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
 
 @WebMvcTest (ReviewController.class)
 public class ReviewControllerMvcTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldBeOkForAllReviewsInTheReviewsTemplate() throws Exception {
+        mockMvc.perform(get("/reviews")).andExpect(status().isOk())
+                .andExpect(view().name("reviewsTemplate"));
+        
+    }
 }
