70126eab61171f6d886f93ec4007dbe16cfd1b6d
Jovahnn Tucker <Jovahnn@yahoo.com>
2021-06-02 21:52:47 -0400

    Controller Test 1 passed

--- src/main/java/org/wecancoeit/reviews/ReviewController.java
+++ src/main/java/org/wecancoeit/reviews/ReviewController.java
@@ -1,8 +1,14 @@
 package org.wecancoeit.reviews;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ReviewController {
+
+    @RequestMapping("/reviews")
+    public String findAllReviews() {
+        return "reviewsTemplate";
+    }
 
 }
--- src/test/java/org/wecancoeit/reviews/ReviewControllerMvcTest.java
+++ src/test/java/org/wecancoeit/reviews/ReviewControllerMvcTest.java
@@ -1,14 +1,14 @@
 package org.wecancoeit.reviews;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 
 @WebMvcTest (ReviewController.class)
