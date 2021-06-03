a01f3f765b6d41763b87b0781eb68968f3800105
Jovahnn Tucker <Jovahnn@yahoo.com>
2021-06-02 22:33:21 -0400

    Controller complete

--- src/test/java/org/wecancoeit/reviews/ReviewControllerMvcTest.java
+++ src/test/java/org/wecancoeit/reviews/ReviewControllerMvcTest.java
@@ -44,4 +44,17 @@ public class ReviewControllerMvcTest {
         mockMvc.perform(get("/reviews")).andExpect(model()
                 .attribute("reviewsModel", allReviewsInModel));
     }
+    @Test
+    public void shouldBeOkForOneReviewInTheReviewTemplate() throws Exception {
+        mockMvc.perform(get("/review?id=1")).andExpect(status().isOk())
+                .andExpect(view().name("reviewTemplate"));
+    }
+    @Test
+    public void shouldFindReviewOneinModel() throws Exception {
+        Long reviewOneId = 1L;
+        when(reviewRepo.findOne(reviewOneId)).thenReturn(reviewOne);
+        mockMvc.perform(get("/review?id=1")).andExpect(model()
+                .attribute("reviewModel", reviewOne));
+
+    }
 }
