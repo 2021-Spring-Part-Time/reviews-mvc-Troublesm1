7d1a7efd086d0e9269276335244da25e42157381
Jovahnn Tucker <Jovahnn@yahoo.com>
2021-06-02 22:15:19 -0400

    Controller find all built out and tested

--- src/test/java/org/wecancoeit/reviews/ReviewControllerMvcTest.java
+++ src/test/java/org/wecancoeit/reviews/ReviewControllerMvcTest.java
@@ -2,13 +2,20 @@ package org.wecancoeit.reviews;
 
 import org.junit.jupiter.api.Test;
 
+import org.mockito.Mock;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @WebMvcTest (ReviewController.class)
@@ -17,10 +24,24 @@ public class ReviewControllerMvcTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private ReviewRepository reviewRepo;
+
+    @Mock
+    private Review reviewOne;
+    @Mock
+    private Review reviewTwo;
+
     @Test
     public void shouldBeOkForAllReviewsInTheReviewsTemplate() throws Exception {
         mockMvc.perform(get("/reviews")).andExpect(status().isOk())
                 .andExpect(view().name("reviewsTemplate"));
-        
+    }
+    @Test
+     public void shouldFindAllReviewsInModel() throws Exception {
+        Collection<Review> allReviewsInModel = Arrays.asList(reviewOne, reviewTwo);
+        when(reviewRepo.findAll()).thenReturn(allReviewsInModel);
+        mockMvc.perform(get("/reviews")).andExpect(model()
+                .attribute("reviewsModel", allReviewsInModel));
     }
 }
